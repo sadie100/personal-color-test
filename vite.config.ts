f8876adba93d3449c846befd8bd63c95aeb46469
sadie100 <03sadie@gmail.com>
2026-05-29 03:00:52 +0900

    chore: vite checker 플러그인 설치(tsc, eslint를 개발중 체크해줌)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), checker({ typescript: true })],
   test: {
     environment: "node",
     globals: true,
